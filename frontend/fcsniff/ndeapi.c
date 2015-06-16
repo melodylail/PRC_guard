b949eb4c0de9bba624955d4d03e9e97a34dad1a3
zhuangfei <zhuang.fei@live.com>
2015-06-16 23:01:05 +0800

    rm comments

--- frontend/fcsniff/ndeapi.c
+++ frontend/fcsniff/ndeapi.c
@@ -1,20 +1,9 @@
 /**
  *****************************************************************************
  *
- *  (C) Copyright 2015-2015 ZERO HACKER.
+ *  (C) Copyright 2015-2015 orange-sys.
  *  All Rights Reserved.
- *
- *  This program is an unpublished copyrighted work which is proprietary
- *  to Trend Micro, Inc. and contains confidential information that is not
- *  to be reproduced or disclosed to any other person or entity without
- *  prior written consent from Trend Micro, Inc. in each and every instance.
- *
- *  WARNING:  Unauthorized reproduction of this program as well as
- *  unauthorized preparation of derivative works based upon the
- *  program or distribution of copies by sale, rental, lease or
- *  lending are violations of federal copyright laws and state trade
- *  secret laws, punishable by civil and criminal penalties.
- *
+ *****************************************************************************
  **/
 
 #include "nde_types.h"
