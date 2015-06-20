bdbdddb6d59326e5e4f4a8602a444018d4e4ee8b
luq <luq@ubuntu.com>
2015-06-21 01:04:19 +0800

    run shell in python with user input channels

--- python_ui/tkinter_test.py
+++ python_ui/tkinter_test.py
@@ -2,19 +2,47 @@
 
 import time
 import Tkinter as tk
-
+import subprocess
 
 class Window:
-    def __init__(self, title='Guard', width=480, height=160, startFunc=bool, stopFunc=bool):
+    def __init__(self, title='Guard', width=480, height=160):
         self.w = width
         self.h = height
         self.stat = True
-        self.startFunc = startFunc
-        self.stopFunc = stopFunc
+#        self.startFunc = startFunc
+#        self.stopFunc = stopFunc
         self.staIco = None
         self.stoIco = None
 
         self.root = tk.Tk(className=title)
+
+    def run_command(self, cmd, *args):
+        if not cmd:
+            return ''
+        if args:
+            cmd = ' '.join((cmd,) + args)
+        p = subprocess.Popen(cmd, shell=True,stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        print (cmd)
+        rf, ef = p.stdout, p.stderr
+        errors = ef.read()
+        if errors:
+            print("ERROR: %s" % errors)
+        return rf.read().strip()
+
+    def start(self):
+        channel = self.channel.get()
+
+        print('start, channel : %s' % channel)
+
+        print self.run_command("ls", "-al")
+
+        print self.run_command("echo", "-c " + channel)
+
+        return True
+
+    def stop(self):
+        print('stop.')
+        return True
 
     def center(self):
         ws = self.root.winfo_screenwidth()
@@ -25,7 +53,7 @@ class Window:
 
 
     def packBtn(self):
-        self.lab = tk.Label(self.root, text='监测信道:')
+        self.lab = tk.Label(self.root, text='监测信道(1,6,11):')
         self.lab.grid(row = 0, column = 0)
         self.channel = tk.Entry(self.root)
         self.channel.grid(row = 0, column = 1, sticky = tk.E)
@@ -38,12 +66,12 @@ class Window:
     def event(self):
         self.btnSer['state'] = 'disabled'
         if self.stat:
-            if self.stopFunc():
+            if self.stop():
                 self.btnSer['text'] = '启动服务'
                 self.stat = False
 #                self.root.iconbitmap(self.stoIco)
         else:
-            if self.startFunc():
+            if self.start():
                 self.btnSer['text'] = '停止服务'
                 self.stat = True
 #                self.root.iconbitmap(self.staIco)
@@ -58,17 +86,11 @@ class Window:
         self.root.mainloop()
 
 ########################################################################
-def start():
-    print('start.')
-    return True
-def stop():
-    print('stop.')
-    return True
 
 if __name__ == '__main__':
     import sys, os
 
-    w = Window(startFunc=start, stopFunc=stop)
+    w = Window()
 #    w.staIco = os.path.join(sys.path[0], 'test.xbm')
 #    w.stoIco = os.path.join(sys.path[0], 'stop.xbm')
     w.loop()
